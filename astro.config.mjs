19b45129ccac32b0396a4350e35bf9e9408ffea5
jay <mengjiewu339@gmail.com>
2026-06-04 11:50:28 +0800

    正式上線版 v1.0.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  base: '/stories-astro/',
+  
+  // 💡 加上下面這段設定，強制把 _astro 改名成 assets！
+  build: {
+    assets: 'assets',
+  },
+});
